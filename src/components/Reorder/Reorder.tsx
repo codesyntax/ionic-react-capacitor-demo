fe85062ac6b01c1fcb48465db3a24ea44d306f2d
bipoza <bipoza@gmail.com>
2022-01-27 09:37:45 +0100

    Reorder bug fixed

--- src/components/Reorder/Reorder.tsx
+++ src/components/Reorder/Reorder.tsx
@@ -6,11 +6,22 @@ import {
     IonList,
     IonListHeader,
     IonToggle,
+    ItemReorderEventDetail,
   } from "@ionic/react";
 import { useState } from "react";
   
   const Reorder: React.FC = () => {
     const [reorderToggle, setReorderToggle] = useState(true);
+    function doReorder(event: CustomEvent<ItemReorderEventDetail>) {
+      // The `from` and `to` properties contain the index of the item
+      // when the drag started and ended, respectively
+      console.log('Dragged from index', event.detail.from, 'to', event.detail.to);
+    
+      // Finish the reorder and position the item in the DOM based on
+      // where the gesture ended. This method can also be called directly
+      // by the reorder group
+      event.detail.complete();
+    }
     return (
       <>
          <IonListHeader mode="ios">
@@ -23,7 +34,7 @@ import { useState } from "react";
           />
         </IonListHeader>
         <IonList>
-          <IonReorderGroup disabled={!reorderToggle}>
+          <IonReorderGroup disabled={!reorderToggle} onIonItemReorder={doReorder}>
             {/*-- Default reorder icon, end aligned items --*/}
             {[...Array(5).keys()].map((e) => (
               <IonItem key={e}>
